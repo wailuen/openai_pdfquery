b0a637776d0bcb960d12427d6190f4b14018e318
wailuen <wailuen.cheong@yahoo.com>
2023-04-18 00:00:45 +0800

    added colon

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -11,7 +11,7 @@ export default async function handler(
 ) {
   const { question, history } = req.body;
 
-  console.log('question', question);
+  console.log('question:', question);
 
   //only accept post requests
   if (req.method !== 'POST') {
@@ -46,10 +46,10 @@ export default async function handler(
       chat_history: history || [],
     });
 
-    console.log('response', response);
+    console.log('**response', response);
     res.status(200).json(response);
   } catch (error: any) {
-    console.log('error', error);
+    console.log('**error', error);
     res.status(500).json({ error: error.message || 'Something went wrong' });
   }
 }
