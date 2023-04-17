53ba050ea2e15dd82145a9c2902646d40e560423
wailuen <wailuen.cheong@yahoo.com>
2023-04-17 21:21:43 +0800

    First commit

---
+++ utils/pinecone-client.ts
@@ -0,0 +1,27 @@
+import { PineconeClient } from '@pinecone-database/pinecone';
+
+if (!process.env.PINECONE_ENVIRONMENT || !process.env.PINECONE_API_KEY) {
+  throw new Error('Pinecone environment or api key vars missing');
+}
+
+async function initPinecone() {
+  try {
+    const pinecone = new PineconeClient();
+
+    console.log('**api', process.env.PINECONE_API_KEY);
+    console.log('**env', process.env.PINECONE_ENVIRONMENT);
+    console.log('**index', process.env.PINECONE_INDEX_NAME);
+    await pinecone.init({
+      environment: process.env.PINECONE_ENVIRONMENT ?? '', //this is in the dashboard
+      // apiKey: 'c3d9cc03-71e5-4202-a96d-1752f3eac37e' ?? '',
+      apiKey: process.env.PINECONE_API_KEY ?? '',
+    });
+
+    return pinecone;
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to initialize Pinecone Client');
+  }
+}
+
+export const pinecone = await initPinecone();
